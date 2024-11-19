23cb24c2d2eafd4747fe50f0d8d5b8c5846cc373
joshuasparkes <joshsparkes6@gmail.com>
2024-11-19 10:43:49 +0200

    be boilerplate

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,13 @@
-# flask
+from flask import Flask, request, jsonify
+import requests
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+if __name__ == "__main__":
+    app.run(port=5000)
